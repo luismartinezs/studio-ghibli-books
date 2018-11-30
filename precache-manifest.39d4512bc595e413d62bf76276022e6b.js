307bd793ee79d68c2825bb97b60e8e41b55d706d
Luis Martinez <martinez.s.luis@gmail.com>
2018-11-30 20:50:46 +0100

    Updates

---
+++ precache-manifest.39d4512bc595e413d62bf76276022e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fd54a790d0b8ff17210a38cccf876d8",
+    "url": "/studio-ghibli-books/static/media/totoro-fields.2fd54a79.jpg"
+  },
+  {
+    "revision": "183f7e2f4084450e2a0ff6a916b84d0f",
+    "url": "/studio-ghibli-books/static/media/book_cover_template.183f7e2f.jpg"
+  },
+  {
+    "revision": "bd1864c243c7c3f08660",
+    "url": "/studio-ghibli-books/static/js/runtime~main.bd1864c2.js"
+  },
+  {
+    "revision": "ba1745165cc7a92240ad",
+    "url": "/studio-ghibli-books/static/js/main.ba174516.chunk.js"
+  },
+  {
+    "revision": "d486a5435639d0860bf3",
+    "url": "/studio-ghibli-books/static/js/1.d486a543.chunk.js"
+  },
+  {
+    "revision": "ba1745165cc7a92240ad",
+    "url": "/studio-ghibli-books/static/css/main.e5286a99.chunk.css"
+  },
+  {
+    "revision": "b349dde634ece8d61d5665accabc05ed",
+    "url": "/studio-ghibli-books/index.html"
+  }
+];
